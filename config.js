323f0c8619fc4f13f10ac7a7d5333c9828f2a0b4
Cris Amarante <amarante_s@hotmail.com>
2026-05-20 22:00:57 -0300

    Update URL_PLANILHA to new Google Script URL

--- config.js
+++ config.js
@@ -5,4 +5,4 @@
 
 //=======================COMENTE SE NÃO ESTIVER EM USO====REMOVA O COMENTÁRIO SE FOR USAR=================================================
 //ambiente de testes de campo
-const URL_PLANILHA = "https://script.google.com/macros/s/AKfycbyAIzgbB5WDx4G8HciCxMnIGNyndT6GJyBprKRUWGE8XM07jmArjYeiYea9ZqwNyc6e/exec";
+const URL_PLANILHA = "https://script.google.com/macros/s/AKfycbyc7ysqd65u-jgkeXywwf2PkdULjpOJyW0CcUg4rLXczMI9eeedC-bOHgzEV0-oXuId/exec";
