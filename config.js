cdf0bc96afc2f5daf302a846469d7dc0c228a31b
Cris Amarante <amarante_s@hotmail.com>
2026-06-02 20:08:23 -0300

    Update URL_PLANILHA to new Google Script URL

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const URL_PLANILHA = "https://script.google.com/macros/s/AKfycby5NlX82q1tCq6Rp3RmjlkRMDWNgPuTlYsgDLVdct1ao-ijmUf9_4nphAwI4cCpec_ngA/exec";
+const URL_PLANILHA = "https://script.google.com/macros/s/AKfycbyHFU6Jt3aP8FVUALMrT7ORMISo8tAR7OUVy_SUGZ4nP8VLDCKd-3Nh3GzRWGu02BXUsA/exec";
