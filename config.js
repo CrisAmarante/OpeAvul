4d7cda19b0618bc3a68f19cca4c434ffe5c0c55f
Cris Amarante <amarante_s@hotmail.com>
2026-06-05 08:43:16 -0300

    Update URL_PLANILHA to new endpoint

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const URL_PLANILHA = "https://script.google.com/macros/s/AKfycby34x9_VZwvBJAuD2SkyTK85BhMja6zANUbXmhYp6wE1u298BpR8ONdSqIPvzbmaUBwyw/exec";
+const URL_PLANILHA = "https://script.google.com/macros/s/AKfycbzeSPd3GIWI7IZTcX_W4vX8h1RCw-Q8HGIRhJDikSOGD6pz0c9k_oYOIANHBI-AD-iSkQ/exec";
